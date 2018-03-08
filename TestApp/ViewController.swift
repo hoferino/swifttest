d63dc9e073921b5d62c9c4252dd107064e02bf9e
Max Höfer👑 <maxhofer@maxs-mbp.fritz.box>
2018-03-08 19:57:47 +0100

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Max Höfer on 08.03.18.
+//  Copyright © 2018 Max Höfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
